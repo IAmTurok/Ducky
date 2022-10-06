37d988df5ccdd3b536159f912c19713bd0b8bd4a
Federico Turati <58822162+IAmTurok@users.noreply.github.com>
2022-10-06 09:25:18 +0200

    Aggiunta commenti

--- Qwack.py
+++ Qwack.py
@@ -3,14 +3,17 @@ from telegram import Update, InlineQueryResultArticle, InputTextMessageContent
 from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes, InlineQueryHandler
 from collections import namedtuple
 
+#
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 
+# List of images to be deleted
 delete_list = []
 
 
+# System to retrieve random images of ducks
 async def random():
     loop = asyncio.get_event_loop()
     session = aiohttp.ClientSession(loop=loop)
@@ -27,7 +30,8 @@ async def random():
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Non servo ad un cazzo, solo coprire le bestemmie")
 
-
+    
+# Main function: delete the message with the blasphemy and send a random photo of ducks
 async def qwack(update: Update, context: ContextTypes.DEFAULT_TYPE):
     random_async = await random()
     await context.bot.delete_message(chat_id=update.effective_chat.id, message_id=update.message.message_id)
@@ -35,12 +39,14 @@ async def qwack(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_photo(chat_id=update.effective_chat.id, photo=random_async.url)
     delete_list.append(update.message.message_id + 1)
 
-
+    
+# Cleaner function, every certain time (see below) it activates and deletes the photos of the ducks
 async def del_qwack(context: ContextTypes.DEFAULT_TYPE):
     if delete_list:
         await context.bot.delete_message(chat_id='-1001283327584', message_id=delete_list.pop())
 
-
+        
+# Function that I needed to keep, has no bearing on the project, you can ignore it
 async def inline_caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.inline_query.query
     if not query:
@@ -52,7 +58,7 @@ async def inline_caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
     )]
     await context.bot.answer_inline_query(update.inline_query.id, results)
 
-
+# Unknown command management function, you can ignore it
 async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
 
@@ -62,9 +68,12 @@ class CouldNotGetDuckError(Exception):
 
 
 if __name__ == '__main__':
-    application = ApplicationBuilder().token('867893257:AAH3rmAkppqD69XdI4G3Fj8sK7Z7GA0BY5g').build()
+    # Put your token below
+    application = ApplicationBuilder().token('PLACE TOKEN HERE').build()
+    # Initialize a job queue
     job_queue = application.job_queue
 
+    # control area
     start_handler = CommandHandler('start', start)
     qwack_Dio_handler = MessageHandler(filters.Regex(re.compile(r'.*[D|d][I|i][O|o].*', re.IGNORECASE)), qwack)
     qwack_Madonna_handler = MessageHandler(filters.Regex(re.compile(r'.*[M|a]donna.*', re.IGNORECASE)), qwack)
@@ -72,12 +81,15 @@ if __name__ == '__main__':
     inline_caps_handler = InlineQueryHandler(inline_caps)
     unknown_handler = MessageHandler(filters.COMMAND, unknown)
 
+    # handeler contol area
     application.add_handler(start_handler)
     application.add_handler(qwack_Dio_handler)
     application.add_handler(qwack_Madonna_handler)
     application.add_handler(qwack_Gesu_handler)
     application.add_handler(inline_caps_handler)
     application.add_handler(unknown_handler)
+    # auto-deletion queue trigger timer
     job_queue.run_repeating(del_qwack, interval=60, first=600)
 
+    # job for continuous polling
     application.run_polling()
